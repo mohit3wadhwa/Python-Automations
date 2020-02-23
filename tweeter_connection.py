aa6c208bd2c4d022f135fb78157d67e5e89e6f08
Mohit Wadhwa <mohit3wadhwa@gmail.com>
2020-02-23 23:11:13 +0000

    remove credentials

--- tweeter_connection.py
+++ tweeter_connection.py
@@ -2,10 +2,10 @@ import tweepy
 
 print("I am Twitter app")
 
-CONSUMER_KEY = 'xpYDhdBj39T5Gzktwrksg8bBH'
-CONSUMER_SECRET = '5SpeKHJ4TIlV8is9l1rhd06h0vSWFiyOx9kWqICQaoGMXywERp' 
-ACCESS_KEY = '4025843547-c8u7fy2mwIPU9C9hi77dpGZ1RJiFg8cM4iz9wfL'
-ACCESS_SECRET = 'aUqb0GKH4tvBkFyHBclT126Bbx9JL5srXMQ2Yo8zlxbiw'
+CONSUMER_KEY = ''
+CONSUMER_SECRET = '' 
+ACCESS_KEY = ''
+ACCESS_SECRET = ''
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
